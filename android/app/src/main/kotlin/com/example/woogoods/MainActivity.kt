2328622a8fda896c2975c0ba41b89e6743ea2c5a
Bhavik kothari <kotharibhavik23@gmail.com>
2023-01-23 21:16:20 +0530

    fixed all bugs of gradle and manifest

---
+++ android/app/src/main/kotlin/com/example/woogoods/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.akaarit.woogoods
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+
+}
